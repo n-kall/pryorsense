e0f51a65e787f5ba1b8b4d14e77ddaf56ba8f386
n-kall <n-kall@github.com>
2022-05-11 12:11:25 +0300

    add function for powerscaling

--- src/pryorsense/powerscale.py
+++ src/pryorsense/powerscale.py
@@ -3,22 +3,18 @@ import numpy as np
 import pymc3 as pm
 from arviz import InferenceData
 
-draws = 500
-chains = 2
+draws = 2000
+chains = 4
 
-eight_school_data = {
-    "J": 8,
-    "y": np.array([28.0, 8.0, -3.0, 7.0, -1.0, 1.0, 18.0, 12.0]),
-    "sigma": np.array([15.0, 10.0, 16.0, 11.0, 9.0, 11.0, 10.0, 18.0]),
+data = {
+    "y": np.array([20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]),
 }
 
 with pm.Model() as model:
-    mu = pm.Normal("mu", mu=0, sd=5)
-    tau = pm.HalfCauchy("tau", beta=5)
-    theta_tilde = pm.Normal("theta_tilde", mu=0, sd=1, shape=eight_school_data["J"])
-    theta = pm.Deterministic("theta", mu + tau * theta_tilde)
+    mu = pm.Normal("mu", mu=0, sd=1)
+    sigma = pm.HalfNormal("sigma", sd=2.5)
     pm.Normal(
-        "obs", mu=theta, sd=eight_school_data["sigma"], observed=eight_school_data["y"]
+        "obs", mu=mu, sd=sigma, observed=data["y"]
     )
 
     trace = pm.sample(draws, chains=chains)
@@ -28,30 +24,45 @@ with pm.Model() as model:
     pm_data = az.from_pymc3(
         trace=trace,
         prior=prior,
-        posterior_predictive=posterior_predictive,
-        coords={"school": np.arange(eight_school_data["J"])},
-        dims={"theta": ["school"], "theta_tilde": ["school"]},
+        posterior_predictive=posterior_predictive
     )
 
 joint_log_lik = np.sum(pm_data.log_likelihood.stack(draws=("chain", "draw")).obs.values, axis = 0)
 
-alpha = 0.5
-log_lik_ps = (alpha - 1) * joint_log_lik
+alpha = 2
 
 new_group = {
-    "powerscale_weights":
-    {"likelihood" :
-     log_lik_ps
-     }
+    "powerscale":
+    {
+        "likelihood" : joint_log_lik,
+        "prior" : joint_log_lik
+    }
 }
 
 
 pm_data.add_groups(new_group)
 
-w = pm_data.powerscale_weights.stack(draws = ("chain", "draw")).likelihood.values
+def powerscale_weights(data, alpha, component):
+
+    if (component is "likelihood"):
+        ps = np.sum(pm_data["log_likelihood"].stack(draws = ("chain", "draw")).obs.values, axis = 0)
+    elif (component is "prior"):
+        ps = pm_data["log_prior"].stack(draws = ("chain", "draw")).values
+    
+    w = (alpha - 1) * ps
+    
+    return w
+
+w = powerscale_weights(pm_data, 5, "likelihood")
+
+wps = az.psislw(w)
 
 mu = pm_data.posterior.stack(draws = ("chain", "draw")).mu.values
 
-base_mean = np.average(mu)
-scaled_mean = np.average(mu, weights = w, axis = 0)
+base_mean = np.average(mu, axis = 0)
+scaled_mean = np.average(mu, weights = np.exp(wps), axis = 0)
 
+np.dot(mu, w)
+
+base_sd = np.sqrt(np.var(mu, axis = 0))
+scaled_sd = np.sqrt(np.average((mu-scaled_mean)**2, weights=np.exp(wps), axis = 0))
